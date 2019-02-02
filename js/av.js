66590231a92cbe8508568bfd6808e9d99e16b492
agohorel <alex.oddlogic@gmail.com>
2019-02-02 15:56:06 -0500

    update button text on click

--- js/av.js
+++ js/av.js
@@ -5,10 +5,12 @@ seeMoreBtn.forEach(function(button){
 	button.addEventListener("click", function(){
 		if (!toggleState){
 			this.parentElement.nextElementSibling.style.display = "block";
+			this.innerText = "see less";
 			toggleState = !toggleState;
 		} else {
 			this.parentElement.nextElementSibling.style.display = "none";
 			toggleState = !toggleState;
+			this.innerText = "see more";
 		}
 	});
 });
