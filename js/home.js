abe56da0fc60e9f94d338e79dc59887e77bb4074
agohorel <alex.oddlogic@gmail.com>
2019-02-05 19:23:02 -0500

    toggle mobilenav off on click

--- js/home.js
+++ js/home.js
@@ -1,8 +1,9 @@
 let hamburger = document.querySelector(".navbar__hamburger-container");
 let mobileNav = document.querySelector(".mobilenav__container");
+let mobileNavItems = document.querySelectorAll(".mobilenav__item");
 let isVisible = false;
 
-hamburger.addEventListener("click", function(){
+hamburger.addEventListener("click", () => {
 	if (!isVisible){
 		mobileNav.style.display = "flex";
 		isVisible = !isVisible;	
@@ -10,5 +11,11 @@ hamburger.addEventListener("click", function(){
 		mobileNav.style.display = "none";
 		isVisible = !isVisible;
 	}
-	
+});
+
+mobileNavItems.forEach((item) => {
+	item.addEventListener("click", () => {
+		mobileNav.style.display = "none";
+		isVisible = false;
+	});
 });
