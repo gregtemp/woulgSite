98473f532c7f08d04a3ee029015a39afd8e57465
agohorel <alex.oddlogic@gmail.com>
2019-02-12 17:59:07 -0500

    rework av section
    full width dropdowns when clicking "see more" instead of weird 1/3 width columns w/ tons of whitespace

--- js/av.js
+++ js/av.js
@@ -2,8 +2,13 @@ let seeMoreButtons = document.querySelectorAll(".newspaper__popout-button");
 
 seeMoreButtons.forEach((button) => {
 	button.addEventListener("click", function(){
-		if (this.parentElement.nextElementSibling){
-			this.parentElement.nextElementSibling.style.display = "block";
+		if (this.parentElement.parentElement.id){
+			// get the id of the containing div column
+			let id = this.parentElement.parentElement.id;
+			// interpolate the desired div selector
+			let divToShow = document.querySelector(`#${id}-popout-container`);
+			// show the div
+			divToShow.style.display = "flex";
 		}
 	});
 });
