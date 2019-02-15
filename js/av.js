521cab5758157fb96a104597dcad801a02fc0216
agohorel <alex.oddlogic@gmail.com>
2019-02-15 16:03:22 -0500

    clear a/v popout containers on "show more" to prevent multiples stacking

--- js/av.js
+++ js/av.js
@@ -1,8 +1,13 @@
 let seeMoreButtons = document.querySelectorAll(".newspaper__popout-button");
+let popoutContainers = document.querySelectorAll(".newspaper__popout-container");
 
 seeMoreButtons.forEach((button) => {
 	button.addEventListener("click", function(){
 		if (this.parentElement.parentElement.id){
+			// clear any existing popouts
+			popoutContainers.forEach((container) => {
+				container.style.display = "none";
+			});
 			// get the id of the containing div column
 			let id = this.parentElement.parentElement.id;
 			// interpolate the desired div selector
